c4158926e74876bb2b2365b32e010728d7e5aa5f
Daniel Paurat <daniel.paurat@gmail.com>
2024-10-29 21:10:29 +0100

    beutification

--- src/app.py
+++ src/app.py
@@ -11,21 +11,27 @@ with open("ressources/config.yaml", "r", encoding="utf-8") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 
+# Go through the config and create for each ``page*`` section a page in the website
 game_pages = []
 for key in config.keys():
     if re.match(r"^page\d+", key):
+        # Get page rendering funktion
         render_page = get_named_page_renderer(
             "render_" + key,
             page_num=int(key[4:]),
             is_start=config[key]["is_start"],
             is_end=config[key]["is_end"],
         )
-        current_page = st.Page(
-            render_page, title=config[key]["title"], icon=":material/arrow_forward_ios:"
+        # Create page
+        game_pages.append(
+            st.Page(
+                render_page,
+                title=config[key]["title"],
+                icon=":material/arrow_forward_ios:",
+            )
         )
-        game_pages.append(current_page)
 
-
+# Add a navigation with two sections
 pg = st.navigation(
     {
         "Scavenger Hunt": game_pages,
